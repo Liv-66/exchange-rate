04bda0cbd8e988d2d687007a2d65538d1ea0f138
Liv Huang <livhuang95@gmail.com>
2021-09-07 15:53:42 +0800

    get exchange rate from xml and parse it

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const ExangeRateController = require('./controllers/ExangeRateController.js');
 
 const app = express();
 const port = 3000;
 
-app.post('/', ExangeRateController);
+app.get('/', ExangeRateController.getCurrencyData);
+// app.post('/', ExangeRateController);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
--- controllers/ExangeRateController.js
+++ controllers/ExangeRateController.js
@@ -1,5 +1,80 @@
-const { exchangeRates } = require('exchange-rates-api');
+const axios = require('axios');
+const xml2js = require('xml2js');
+const async = require('async');
 
-module.exports = () => {
-    console.log('function');
-}
+parser = new xml2js.Parser();
+
+module.exports = {
+  getCurrencyData: (req, res) => {
+    try {
+      let param = {};
+      let rs = {};
+      async.waterfall(
+        [
+          (next) => {
+            axios
+              .get('http://www.floatrates.com/daily/twd.xml')
+              .then((data) => {
+                if (data.status === 200) {
+                  rs.data = data.data;
+                } else {
+                  rs.error_msg =
+                    'Get currency error, status code: ' + data.status;
+                }
+                next(null, rs);
+              })
+              .catch((e) => {
+                rs.error_msg = e.message;
+                next(rs);
+              });
+          },
+          (rs, next) => {
+            parser.parseString(rs.data, (err, rst) => {
+              if (err) {
+                rs.error_msg = err.toString();
+                next(rs);
+              } else {
+                //   console.log(JSON.stringify(rst.channel.item[0]));
+                rs.currency_ar = [];
+                rs.update_time = rst.channel.lastBuildDate;
+                rs.description = rst.channel.description;
+                rst.channel.item.forEach((el) => {
+                  rs.currency_ar.push(el.targetCurrency[0]);
+                });
+
+                next(null, rs);
+                //   rs.base_currency = rst.channel.baseCurrency[0];
+                //   rs.target_currency = rst.channel.targetCurrency[0];
+                //   rs.exchange_rate = rst.channel.exchangeRate[0];
+                //   rs.inverse_rate = rst.channel.inverseRate[0];
+
+                //   console.log(rs);
+
+                //   console.log(rs);
+
+                //   console.log(JSON.stringify(rst['gesmes:Envelope'].Cube));
+                //   console.log(rst['gesmes:Envelope'].Cube[0].Cube);
+                //   console.log('1: ', rst['gesmes:Envelope'].Cube[0].Cube[0].Cube);
+                //   console.log('2: ', rst['gesmes:Envelope'].Cube[0].Cube[0].$);
+              }
+            });
+          },
+        ],
+        function (error, result) {
+          if (error) {
+            res.send(error.error_msg);
+          } else {
+            param.update_time = result.update_time;
+            param.currency_ar = result.currency_ar;
+            param.description = result.description;
+            res.send('controller finish ' + JSON.stringify(param));
+          }
+          console.log('error: ', error);
+          console.log('result: ', result);
+        }
+      );
+    } catch (e) {
+      res.send('getCurrencyData catch in ' + e.toString());
+    }
+  },
+};
