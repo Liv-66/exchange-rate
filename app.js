355b3a8a23d5d7a59939f7b609edf8fbd802afb0
Liv Huang <livhuang95@gmail.com>
2021-09-08 18:01:56 +0800

    set port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const ExangeRateController = require('./controllers/ExangeRateController.js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.engine(
   'hbs',
