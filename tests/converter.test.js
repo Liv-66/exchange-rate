9eceac2650c5e724ea90ff74065162f4f35c85c2
liv.huang <livhuang95@gmail.com>
2021-11-03 17:34:38 +0800

    modified tests files

--- tests/converter.test.js
+++ tests/converter.test.js
@@ -4,6 +4,23 @@ const sinon = require('sinon');
 const should = chai.should();
 
 const app = require('../app');
+const ExchangeRateController = require('../controllers/ExchangeRateController.js');
+var assert = chai.assert;
+
+describe(" TEST saving function", function() {
+  
+  it("check if function verifyData() return false", function(){
+      var result = ExchangeRateController.verifyData('test', 'wrong', 'input');
+      assert.isFalse(result);
+  })
+  
+  it("check if result of funtion verifyData() is expected", function(){
+      var result = ExchangeRateController.verifyData('EUR', '100', 'TWD', '3,231');
+      var expected = 'EUR, "100", =, TWD, "3,231"\n';
+      assert.deepEqual( result, expected)
+  })
+
+})
 
 describe('# TEST', function () {
   context('# [取得匯率]', () => {
